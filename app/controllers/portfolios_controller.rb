0d0e3018b2a570da30a95d8da263420c8152e49a
Michal Wiacek <thepowerofra@gmail.com>
2018-04-15 18:17:55 +0200

    Implemented destroy action for porfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -42,7 +42,7 @@ class PortfoliosController < ApplicationController
   def destroy
     @portfolio_item.destroy
     respond_to do |format|
-      format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully destroyed.' }
+      format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
